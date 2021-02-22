9c35b90a82a0d5172d01e02ef6b791e3bcd0a70c
Taylor Friesen <taylorfriesen@protonmail.com>
2021-02-22 15:45:34 -0800

    clicking movie details opens movie page

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,20 +1,22 @@
 import React from 'react';
+import {Link, useHistory} from 'react-router-dom';
 
 export default function MovieList(props) {
+  
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <MovieDetails key={movie.id} movie={movie}/>
       ))}
     </div>
   );
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
-
+  const { title, director, metascore, id } = props.movie;
+  const history = useHistory();
   return (
-    <div className="movie-card">
+    <div className="movie-card" onClick={() => history.push(`/movies/${id}`)}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
